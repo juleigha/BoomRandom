0cd413d10c2fef2f07ce3db4669341abeae98885
juleigha <juleighaquist@gmail.com>
2022-06-06 16:11:38 -0400

    Update boomMain.js

--- boomMain.js
+++ boomMain.js
@@ -1,16 +1,17 @@
+// This small program will only load jpegs the user is approaching
+
 $(document).ready(function() {
 	const approaching = new Event('load:Img');
 	const receding  = new Event('unload:Img');
 	var lastPane =  ( localStorage.getItem("LastPane") == "null" || localStorage.getItem("LastPane") == null )? "pane0" : localStorage.getItem("LastPane") ;
-	// ScrollToElem();
+	
+	// Scroll to last element
 	document.querySelector("[pane-ID="+lastPane+"]").scrollIntoView();
 	$("[pane-ID="+lastPane+"]").attr("src",$("[pane-ID="+lastPane+"]").attr("pane-src"));
-// 	setTimeout(function() {
-// 		$(".comic-wrapper img").css("height",$("[pane-ID="+lastPane+"]").width() * 1.6 );
-// 		console.log($("[pane-ID="+lastPane+"]").width() * 1.6 );
-// },200);
 
 	var imgs = $(".comic-wrapper img");
+	
+	// updates source of images as img element approaches 
 	$(document).scroll(function(){
 		if($(document).scrollTop() % 100 < 10){
 			for(var i = 0; i<imgs.length;i++){
@@ -27,6 +28,8 @@ $(document).ready(function() {
 			}
 		}
 	});
+	
+	// open nav 
 	$(".chapter-btn").click(function(){
 		if($("#contentsTable").hasClass("Active")){
 			closeContents(document.querySelector("[pane-ID="+lastPane+"]"));
@@ -35,15 +38,21 @@ $(document).ready(function() {
 			openContents();
 		}
 	})
+	
+	// chapter nav links
 	$("#contentsTable li a").click(function(e){
 		e.preventDefault();
 		var id = $(this).attr("data-scroll");
 		document.getElementById(id).children[0].children[0].dispatchEvent(approaching);
 		closeContents(document.getElementById(id));
 	})
+	
+	//update image source 
 	$("img").on('load:Img',function(){
 		$(this).attr("src",$(this).attr("pane-src"));
 	})
+	
+	// remove image from page (if site is larg enough) 
 	$("img").on("unload:Img", function(){
 		$(this).attr("src","");
 	})
@@ -77,26 +86,5 @@ $(document).keyup(function(e) {
 	}
 })
 
-var scroll = window.pageYOffset;
-var shadowOffSetDiff = 0;
-$(document).scroll(function(e) {
-	var shadowBlur;
-	var shadowRGB = "rgba(0, 0, 0, 0.8)";
-	var lightSource = window.pageYOffset - 20;
-	shadowOffSetDiff -=1;
-	$("body .overlay").css("opacity",window.pageYOffset/600);
-	$(".lightSource-shadow").each(function() {
-		var hgt = $(this).height();
-		var shadowOffSet = (this.offsetTop - (lightSource ) ) / 35;
-		if (shadowOffSet > -2 && shadowOffSet < 2){
-			$(this).css("box-shadow","0px "+(shadowOffSet)+"px 8px rgba(0, 0, 0, 0.50)" );
-		}
-		else {
-			$(this).css("box-shadow","0px "+(shadowOffSet)+"px 7px rgba(0, 0, 0, 0.51)" );
-			//rgb(23,21,0)
-		}
-	})
-	scroll = window.pageYOffset;
-})
 
 });
